b6303c911611f77d9a90713124ab19fe44d6427b
Perry Fardella <perry.fardella@live.com>
2024-01-01 16:13:32 +0800

    Clean up config and remove unneeded items

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,20 +14,20 @@ const links: {
   label: string;
 }[] = [
   {
-    href: "/#pricing",
-    label: "Pricing",
+    href: "/#about",
+    label: "About",
   },
   {
-    href: "/#testimonials",
-    label: "Reviews",
+    href: "/#projects",
+    label: "Projects",
   },
   {
-    href: "/#faq",
-    label: "FAQ",
+    href: "/writing",
+    label: "Writing",
   },
 ];
 
-const cta: JSX.Element = <ButtonSignin extraStyle="btn-primary" />;
+// const cta: JSX.Element = <ButtonSignin extraStyle="btn-primary" />;
 
 // A header with a logo on the left, links in the center (like Pricing, etc...), and a CTA (like Get Started or Login) on the right.
 // The header is responsive, and on mobile, the links are hidden behind a burger button.
@@ -105,7 +105,7 @@ const Header = () => {
         </div>
 
         {/* CTA on large screens */}
-        <div className="hidden lg:flex lg:justify-end lg:flex-1">{cta}</div>
+        {/* <div className="hidden lg:flex lg:justify-end lg:flex-1">{cta}</div> */}
       </nav>
 
       {/* Mobile menu, show/hide based on menu state. */}
@@ -172,7 +172,7 @@ const Header = () => {
             </div>
             <div className="divider"></div>
             {/* Your CTA on small screens */}
-            <div className="flex flex-col">{cta}</div>
+            {/* <div className="flex flex-col">{cta}</div> */}
           </div>
         </div>
       </div>
--- config.ts
+++ config.ts
@@ -3,11 +3,11 @@ import { ConfigProps } from "./types/config";
 
 const config = {
   // REQUIRED
-  appName: "ShipFast",
+  appName: "Perry Fardella",
   // REQUIRED: a short description of your app for SEO tags (can be overwritten)
-  appDescription:
-    "The NextJS boilerplate with all you need to build your SaaS, AI tool, or any other web app.",
+  appDescription: "The personal website of Perry Fardella.",
   // REQUIRED (no https://, not trialing slash at the end, just the naked domain)
+  // TODO - add domian name here once purchased
   domainName: "shipfa.st",
   crisp: {
     // Crisp website ID. IF YOU DON'T USE CRISP: just remove this => Then add a support email in this config file (mailgun.supportEmail) otherwise customer support won't work.
@@ -15,62 +15,63 @@ const config = {
     // Hide Crisp by default, except on route "/". Crisp is toggled with <ButtonSupport/>. If you want to show Crisp on every routes, just remove this below
     onlyShowOnRoutes: ["/"],
   },
-  stripe: {
-    // Create multiple plans in your Stripe dashboard, then add them here. You can add as many plans as you want, just make sure to add the priceId
-    plans: [
-      {
-        // REQUIRED — we use this to find the plan in the webhook (for instance if you want to update the user's credits based on the plan)
-        priceId:
-          process.env.NODE_ENV === "development"
-            ? "price_1Niyy5AxyNprDp7iZIqEyD2h"
-            : "price_456",
-        //  REQUIRED - Name of the plan, displayed on the pricing page
-        name: "Starter",
-        // A friendly description of the plan, displayed on the pricing page. Tip: explain why this plan and not others
-        description: "Perfect for small projects",
-        // The price you want to display, the one user will be charged on Stripe.
-        price: 99,
-        // If you have an anchor price (i.e. $29) that you want to display crossed out, put it here. Otherwise, leave it empty
-        priceAnchor: 149,
-        features: [
-          {
-            name: "NextJS boilerplate",
-          },
-          { name: "User oauth" },
-          { name: "Database" },
-          { name: "Emails" },
-        ],
-      },
-      {
-        priceId:
-          process.env.NODE_ENV === "development"
-            ? "price_1O5KtcAxyNprDp7iftKnrrpw"
-            : "price_456",
-        // This plan will look different on the pricing page, it will be highlighted. You can only have one plan with isFeatured: true
-        isFeatured: true,
-        name: "Advanced",
-        description: "You need more power",
-        price: 149,
-        priceAnchor: 299,
-        features: [
-          {
-            name: "NextJS boilerplate",
-          },
-          { name: "User oauth" },
-          { name: "Database" },
-          { name: "Emails" },
-          { name: "1 year of updates" },
-          { name: "24/7 support" },
-        ],
-      },
-    ],
-  },
+  // stripe: {
+  //   // Create multiple plans in your Stripe dashboard, then add them here. You can add as many plans as you want, just make sure to add the priceId
+  //   plans: [
+  //     {
+  //       // REQUIRED — we use this to find the plan in the webhook (for instance if you want to update the user's credits based on the plan)
+  //       priceId:
+  //         process.env.NODE_ENV === "development"
+  //           ? "price_1Niyy5AxyNprDp7iZIqEyD2h"
+  //           : "price_456",
+  //       //  REQUIRED - Name of the plan, displayed on the pricing page
+  //       name: "Starter",
+  //       // A friendly description of the plan, displayed on the pricing page. Tip: explain why this plan and not others
+  //       description: "Perfect for small projects",
+  //       // The price you want to display, the one user will be charged on Stripe.
+  //       price: 99,
+  //       // If you have an anchor price (i.e. $29) that you want to display crossed out, put it here. Otherwise, leave it empty
+  //       priceAnchor: 149,
+  //       features: [
+  //         {
+  //           name: "NextJS boilerplate",
+  //         },
+  //         { name: "User oauth" },
+  //         { name: "Database" },
+  //         { name: "Emails" },
+  //       ],
+  //     },
+  //     {
+  //       priceId:
+  //         process.env.NODE_ENV === "development"
+  //           ? "price_1O5KtcAxyNprDp7iftKnrrpw"
+  //           : "price_456",
+  //       // This plan will look different on the pricing page, it will be highlighted. You can only have one plan with isFeatured: true
+  //       isFeatured: true,
+  //       name: "Advanced",
+  //       description: "You need more power",
+  //       price: 149,
+  //       priceAnchor: 299,
+  //       features: [
+  //         {
+  //           name: "NextJS boilerplate",
+  //         },
+  //         { name: "User oauth" },
+  //         { name: "Database" },
+  //         { name: "Emails" },
+  //         { name: "1 year of updates" },
+  //         { name: "24/7 support" },
+  //       ],
+  //     },
+  //   ],
+  // },
   aws: {
     // If you use AWS S3/Cloudfront, put values in here
     bucket: "bucket-name",
     bucketUrl: `https://bucket-name.s3.amazonaws.com/`,
     cdn: "https://cdn-id.cloudfront.net/",
   },
+  // TODO Update this if we decide to add a mailing list
   mailgun: {
     // subdomain to use when sending emails, if you don't have a subdomain, just remove it. Highly recommended to have one (i.e. mg.yourdomain.com or mail.yourdomain.com)
     subdomain: "mg",
@@ -85,17 +86,17 @@ const config = {
   },
   colors: {
     // REQUIRED — The DaisyUI theme to use (added to the main layout.js). Leave blank for default (light & dark mode). If you any other theme than light/dark, you need to add it in config.tailwind.js in daisyui.themes.
-    theme: "light",
+    theme: "dark",
     // REQUIRED — This color will be reflected on the whole app outside of the document (loading bar, Chrome tabs, etc..). By default it takes the primary color from your DaisyUI theme (make sure to update your the theme name after "data-theme=")
     // OR you can just do this to use a custom color: main: "#f37055". HEX only.
     main: themes[`[data-theme=light]`]["primary"],
   },
-  auth: {
-    // REQUIRED — the path to log in users. It's use to protect private routes (like /dashboard). It's used in apiClient (/libs/api.js) upon 401 errors from our API
-    loginUrl: "/api/auth/signin",
-    // REQUIRED — the path you want to redirect users after successfull login (i.e. /dashboard, /private). This is normally a private page for users to manage their accounts. It's used in apiClient (/libs/api.js) upon 401 errors from our API & in ButtonSignin.js
-    callbackUrl: "/dashboard",
-  },
+  // auth: {
+  //   // REQUIRED — the path to log in users. It's use to protect private routes (like /dashboard). It's used in apiClient (/libs/api.js) upon 401 errors from our API
+  //   loginUrl: "/api/auth/signin",
+  //   // REQUIRED — the path you want to redirect users after successfull login (i.e. /dashboard, /private). This is normally a private page for users to manage their accounts. It's used in apiClient (/libs/api.js) upon 401 errors from our API & in ButtonSignin.js
+  //   callbackUrl: "/dashboard",
+  // },
 } as ConfigProps;
 
 export default config;
